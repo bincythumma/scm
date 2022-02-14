314b024f623c66b5698a68f8cef73cd92259900e
bincy <bincy@gmail.com>
2022-02-14 09:37:58 +0000

    mt third commit added method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 class HelloWorld {
+	public int Set(int a) 
+	{
+	}
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+
 	System.out.println("iam Bincy");
-    }
+         }
 }
 
